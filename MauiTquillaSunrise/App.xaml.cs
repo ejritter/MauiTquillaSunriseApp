2f7dc5368688d614a2f2a64ad593e6874feabe2f
Eric Ritter <ejritter87@gmail.com>
2024-11-27 00:42:08 -0500

    Publishable.

--- MauiTquillaSunrise/App.xaml.cs
+++ MauiTquillaSunrise/App.xaml.cs
@@ -5,13 +5,13 @@ public partial class App : Application
     public App()
     {
         InitializeComponent();
-        MainPage = new AppShell();
     }
 
     protected override Window CreateWindow(IActivationState? activationState)
     {
 
         Window window = base.CreateWindow(activationState);
+        window.Page = new AppShell();
         window.Width = 600;
         window.Title = "Maui T-Quilla Sunrise";
         return window;
--- MauiTquillaSunrise/GlobalUsing.cs
+++ MauiTquillaSunrise/GlobalUsing.cs
@@ -5,11 +5,16 @@ global using CommunityToolkit.Mvvm.Input;
 
 global using System.Collections.ObjectModel;
 
-global using Microsoft.Maui.Controls;
+//global using Microsoft.Maui.Controls;
 global using System.Text;
 global using Microsoft.Extensions.Logging;
 
+global using MauiTquillaSunrise;
 global using MauiTquillaSunrise.ViewModel;
 global using MauiTquillaSunrise.Model;
 global using MauiTquillaSunrise.View;
 global using MauiTquillaSunrise.Utility;
+
+
+global using Microsoft.Maui;
+global using Microsoft.Maui.Hosting;
